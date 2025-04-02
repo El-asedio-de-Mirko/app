e09a6216cd8122df80c2fb9da2a4e53f31493098
Alexa020204 <heartPink303@gmail.com>
2025-04-02 00:19:41 -0500

    Agregue la barra de vida

---
+++ Assets/Scripts/Bar/LifeBar.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBar : MonoBehaviour
+{
+    public Slider slider;
+    public float maxHealt;
+    public CharacterScriptableObjects characterData;
+    private void Awake()
+    {
+        maxHealt = characterData.Health;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.maxValue = maxHealt;
+        slider.value = maxHealt;
+    }
+
+    public void DisminuirVida(float val)
+    {
+        slider.value -= val;
+    }
+
+    public void AumentarVida(float val)
+    {
+        slider.value += val;
+    }
+}
